49e2d4fbfc225db7e37c345e164db27fe2483fae
mgporter <26415062+mgporter@users.noreply.github.com>
2023-07-31 10:13:34 -0400

    initial setup complete

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './basestyle.css';
+import './style.css';
+
+console.log('successful! YES!');
+
+const double = (n) => n * 2;
+
+console.log(double(5));
+
+module.exports = double;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: false,
+  },
+  devServer: {
+    static: './dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
